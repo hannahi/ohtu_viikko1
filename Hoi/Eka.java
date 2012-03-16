939241ef938f4fa7d3c942eaa5b47b1b38132ac6
Hanna Hirvonen <hanna.hirvonen@cs.helsinki.fi>
2012-03-16 11:10:47 +0200

    poisto

--- Hoi/Eka.java
+++
@@ -1,7 +0,0 @@
-public class Eka {
-  public static void main(String[] args) {
-    System.out.println("Hyv‰‰ p‰iv‰‰!");
-    System.out.println("Min‰ olen tietokone.");
-    System.out.println("Luulethan, ett‰ olen hyvin viisas?");
-  }
-}
